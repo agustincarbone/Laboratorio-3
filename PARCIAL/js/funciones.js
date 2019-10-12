08e913e942eb2b8fb99c90f2de95d3b430722611
agustincarbone <36281596+agustincarbone@users.noreply.github.com>
2019-10-11 21:19:17 -0300

    parcial

---
+++ PARCIAL/js/funciones.js
@@ -0,0 +1,156 @@
+window.onload = CargarLista;
+// window.addEventListener("load", Guardar);
+// window.addEventListener("load", Mostrar);
+// window.addEventListener("load", editar);
+
+
+var aux=true;
+
+function Mostrar()
+{
+    var boton = document.getElementById("agregar");
+    boton.onclick = MostrarFormulario;
+}
+
+function MostrarFormulario ()
+{
+    var formulario = document.getElementById("contenedor");
+    console.dir(formulario);
+    if(formulario)
+    {
+        aux=!aux;
+    }
+
+    formulario.hidden=aux;
+}
+
+//Carga la lista del servidor en un objeto de tipo JSON
+function CargarLista()
+{
+    var http = new XMLHttpRequest();
+    var persona;
+    var lista;
+    http.open("GET", "http://localhost:3000/personas", true);
+        http.onreadystatechange = function()
+        {
+            if (http.readyState ===4 && http.status ===200){
+                //lo que hace es devolver un objeto de tipo string
+                persona = http.responseText;
+                lista = JSON.parse(persona);
+                generadorLista(lista);
+            }
+        }
+        http.send();
+}
+
+//Carga en la tabla los datos ingresados por el formulario
+function GuardarNombreApellido ()
+{
+    console.log("Entro a guardar");
+    var nombre = document.getElementById("name");
+    var apellido = document.getElementById("apellido");
+    var telefonos = document.getElementById("telefono");
+    var dates = document.getElementById("fecha");
+
+    var fila = "<tr><td>"+nombre.value+"</td><td>"+apellido.value+"</td><td>"+telefonos.value+"</td><td>"+dates.value+"</td><td><a href='' onclick='borrar(event)'>Borrar</a><a href=''onclick='editar(event)'>Editar</a></td></tr>";
+        
+    console.log(fila);
+    document.getElementById("tabla").innerHTML +=fila;
+}
+
+//Recorre uno a uno los elementos de tipo JSON cargados en la variable que se pasa como parametro
+function generadorLista(personas)
+{
+    personas.forEach(element => {
+        GuardarDatosServidor(element);
+    });
+           
+}
+
+//Carga en la tabla los datos del servidor
+function GuardarDatosServidor (element)
+{
+    var nombre;
+    var apellido;
+    var sexo;
+
+    if (element)
+    {
+        console.log("Entro a guardar el parser");
+        nombre = element.nombre;
+        apellido = element.apellido;
+        sexo = element.sexo;
+        img = "<img src='../img/user.png'";
+        var fila = "<div class='card' id='card'><br>"+img+"<br>"+"<label for='nombre'>"+nombre+"</label><br>"+
+        "<label for='apellido'>"+apellido+"</label><br>"+"<label for='sexo'>"+sexo+"</label><br></div>";
+    }
+    else
+    {
+        console.log("No se cargaron los datos");
+    }
+    document.getElementById("cards").innerHTML +=fila;
+
+}
+
+function Guardar()
+{
+    var boton = document.getElementById ("guardar");
+    console.log("guardó");
+    boton.onclick = GuardarNombreApellido;
+    window.addEventListener("Load", guardarEnServidor);
+}
+
+function guardarEnServidor(e)
+{
+    e.preventDefault();
+    var nombre = document.getElementById("name");
+    var apellido = document.getElementById("apellido");
+    var telefonos = document.getElementById("telefono");
+    var dates = document.getElementById("fecha");
+    var objJson = {"nombre": nombre, "apellido": apellido, "fecha": dates, "telefono": telefono};
+    var http = new XMLHttpRequest();
+    http.open('POST', "http://localhost:3000/nuevaPersona", true);
+    http.onreadystatechange = function()
+    {
+        if (http.readyState === 4 && http.status === 200)
+        {
+            var stringDatos = JSON.stringify(abjJson);
+        }
+    }   
+    http.setRequestHeader("Content-Type","application/json");
+    http.send(stringDatos);
+}
+
+function editar(e)
+{
+    e.preventDefault();
+    var nombre = e.target.parentNode.parentNode.children[0].innerHTML;
+    var apellido = e.target.parentNode.parentNode.children[1].innerHTML;
+
+    var formulario = document.getElementById("contenedor");
+    formulario.hidden = false;
+    n = document.getElementById("name");
+    a = document.getElementById("apellido");
+    n.value = nombre;
+    a.value = apellido;
+    // if()
+    e.target.parentNode.parentNode.remove();
+}
+
+function borrar(e)
+{
+    e.preventDefault();
+    var nombre = e.target.parentNode.parentNode.children[0].innerHTML;
+    var apellido = e.target.parentNode.parentNode.children[1].innerHTML;
+    e.target.parentNode.parentNode.remove();
+   
+    // /*Lo que hace es prevenir que se produzca la acción por defecto al presionar*/
+    // e.preventDefault();
+    // /*Obtengo el elemento*/ 
+    // console.log(e.target);
+    // /*Obtengo al padre*/
+    // console.log(e.target.parentNode);
+    // /*Así sucesivamente*/
+    // console.log(e.target.parentNode.parentNode);
+    alert("Se borro una persona");
+}
